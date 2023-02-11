411f367a4d04bc9f516e6f8e6e2d0f6720da9f10
tushar <tusharkanti647@gmail.com>
2023-02-11 17:29:06 +0000

    some part

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
+import PostDetail from './PostDetail';
 
 const posts = [
-    { name: 'Post 1', path: '/posts/1' },
-    { name: 'Post 2', path: '/posts/2' },
-    { name: 'Post 3', path: '/posts/3' },
-    { name: 'Post 4', path: '/posts/4' }
+  { name: 'Post 1', path: '/posts/1' },
+  { name: 'Post 2', path: '/posts/2' },
+  { name: 'Post 3', path: '/posts/3' },
+  { name: 'Post 4', path: '/posts/4' }
 ];
 export default function PostList() {
-    return (
-      <div className="posts">
-        <h1>Post List</h1>
-        <ul>
-          {/* Render a list of Links from the posts object   */}
-        </ul>
-      </div>
-    )
-  }
+  return (
+    <div className="posts">
+      <h1>Post List</h1>
+      <ul>
+        {posts.map((post, ind) => (<li key={ind}><Link to={post.path}>{post.name}</Link> <Switch><Route exact path={post.path} component={PostDetail} /></Switch></li>))}
+        {/* Render a list of Links from the posts object   */}
+      </ul>
+
+      {/* <Switch>
+        {posts.map((post, ind) => <Route exact path={post.path} component={PostDetail} />)
+
+        } */}
+    </Switch>
+    </div >
+  )
+}
